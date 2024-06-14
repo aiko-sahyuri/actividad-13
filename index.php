ce8d5cc420595f7c4bf1d7ebb3987f5f95d1322f
aiko-sahyuri <160526894+aiko-sahyuri@users.noreply.github.com>
2024-06-14 03:09:58 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo $a = 5;
+    $b = 5.34;
+    $c = "25";
+
+    var_dump($a);
+    echo "<br>";
+    var_dump($b);
+    echo "<br>";
+    var_dump($c);
+    ?>  
+
+</html>
